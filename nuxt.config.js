a8d78f7b233791c14c785a0d13994fde707b94cf
ripazhakgggdkp <ripazhakgggdkp@users.noreply.github.com>
2020-10-11 02:47:09 -0500

    track pageviews

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,8 +51,17 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-180295859-1'
+    }],
   ],
+  googleAnalytics: {
+    debug: {
+      enabled: true,
+      sendHitTask: true
+    }
+  },
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
